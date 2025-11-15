01721cc9a56ca63cfe00c6717beb0c2a1bd87577
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2025-11-15 08:14:58 -0500

    Update lambda2.cpp

--- lambda2.cpp
+++ lambda2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cctype>
 using namespace std;
 
 char* FuncCad(char* cad, char (*F)(char)) {
@@ -26,51 +25,21 @@ int main() {
   
     char palabra[]  = "HolaMundo";
     char palabra2[] = "HolaMundo";
-
-    auto UPPER = [](char c)->char { return toupper((unsigned char)c); };
-    auto LOWER = [](char c)->char { return tolower((unsigned char)c); };
-
-    cout << "Upper Case:  " << FuncCad(palabra,  UPPER) << endl;
-    cout << "Lower Case:  " << FuncCad(palabra2, LOWER) << endl;
-
-    return 0;
-}
-
-
-#include <iostream>
-#include <string>
-#include <cctype>
-using namespace std;
-
-string FuncCad(const string &cad, char (*F)(char)) {
-
-    string resultado = cad;
-    char *ini = &resultado[0];
-    int tam = 0;
-    char* p=ini;
-    while (*p != '\0') {
-    p++;
-    tam++;}
-    char *fin = ini + tam - 1;
-
-    for (char *q = ini; q <= fin; q++) {
-        *q = F(*q);
-    }
-
-    return resultado;
-}
-
-int main() {
-    string palabra = "HolaMundo";
-
-    auto UPPER = [](char c)->char { return toupper((char)c); };
-    auto LOWER = [](char c)->char { return tolower((char)c); };
+    auto Upper = [](char c) -> char {
+    if (c >= 'a' && c <= 'z')
+        return c - 32;
+    return c;
+      
+    };
+    auto Lower = [](char c) -> char {
+    if (c >= 'A' && c <= 'Z')
+        return c + 32;
+    return c;
+    };
 
 
-    cout << "Upper Case:  " << FuncCad(palabra, UPPER) <<endl;
-    cout << "Lower Case:  " << FuncCad(palabra, LOWER) <<endl;
+    cout << "Upper Case:  " << FuncCad(palabra,  Upper) << endl;
+    cout << "Lower Case:  " << FuncCad(palabra2, Lower) << endl;
 
     return 0;
 }
-
-
